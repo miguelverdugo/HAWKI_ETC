aed5cbbb53df8773fa1911f66600484168e1bdd4
Miguel Verdugo <mverduol@gmail.com>
2022-11-20 15:23:21 +0100

    some name changes

--- hawki_etc/__init__.py
+++ hawki_etc/__init__.py
@@ -1,7 +1,7 @@
 import os
 import inspect
 
-from .hawki_etc import HAWKI_ETC, ETC_base
+from .etc import HAWKI_ETC, ETC_base
 
 __pkg_dir__ = os.path.dirname(inspect.getfile(inspect.currentframe()))
 __data_dir__ = os.path.join(__pkg_dir__, "data")
